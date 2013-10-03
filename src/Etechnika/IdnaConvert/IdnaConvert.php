cac9b628f3edd6512723f8fb402b32081aa93d15
Tomasz Rutkowski <tomasz.rutkowski@domeny.pl>
2013-10-03 12:11:56 +0200

    add: unit test

--- src/Etechnika/IdnaConvert/IdnaConvert.php
+++ src/Etechnika/IdnaConvert/IdnaConvert.php
@@ -1,11 +1,45 @@
 <?php
 namespace Etechnika\IdnaConvert;
 
-require_once( __DIR__ . DIRECTORY_SEPARATOR. '..' . DIRECTORY_SEPARATOR .'phlylabs' . DIRECTORY_SEPARATOR .'idna_convert.class.php' );
+require_once( __DIR__ . DIRECTORY_SEPARATOR. '..' . DIRECTORY_SEPARATOR .'..'. DIRECTORY_SEPARATOR .'phlylabs' . DIRECTORY_SEPARATOR .'idna_convert.class.php' );
+use \idna_convert;
 
 /**
  * Converter
  */
 class IdnaConvert extends idna_convert
 {
+    /**
+     * Return instance
+     *
+     * @return IdnaConvert
+     */
+    public static function get()
+    {
+        return new static();
+    }
+
+
+    /**
+     * Decode domain name
+     *
+     * @param string $strValue
+     * @return string
+     */
+    public static function decodeString( $strValue )
+    {
+        return static::get()->decode( $strValue );
+    }
+
+
+    /**
+     * Encode domain name
+     *
+     * @param string $strValue
+     * @return string
+     */
+    public static function encodeString( $strValue )
+    {
+        return static::get()->encode( $strValue );
+    }
 }
---
+++ test/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * This file is part of the Etechnika package.
+ *
+ * (c) Tomasz Rutkowski <tomasz@rutkowski.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$loader = require __DIR__ . "/../../../autoload.php";
+$loader->add('Etechnika\\', __DIR__ . DIRECTORY_SEPARATOR .'..'. DIRECTORY_SEPARATOR .'src');
